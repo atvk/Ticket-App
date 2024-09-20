cf06cde7523a8f460a69b0a7b12f85ef6125ef09
Anton Steblovskiy <steblovskiyanton@yandex.ru>
2024-09-20 19:02:32 +0300

    create app

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # Ticket-App
-NextJS 13.4 App  
-
 Tech Stack
 
+NextJS 13.4 App  
+npx create-next-app@latest
+
 Tailwind CSS 
+
 Phosphor-icons
+npm i @phosphor-icons/react
+import { Fish } from "@phosphor-icons/react/dist/ssr";
+const MyServerComponent = () => {return <Fish/>};
+
 MongoDB
+
 Mongoose
 
---
+++ app/(components)/PriorityDisplay.jsx
@@ -0,0 +1,30 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+import { BellSimple } from "@phosphor-icons/react/dist/ssr"
+export default function PriorityDisplay({priority}) {
+  return (
+    <div className="flex justify-start align-baseline ">
+      <BellSimple 
+      size={32} 
+      className={`pr-1 ${priority>0?"text-nav":"text-slate-400"}`}
+      />
+      <BellSimple 
+      size={32} 
+      className={`pr-1 ${priority>1?"text-nav":"text-slate-400"}`}
+      />
+      <BellSimple 
+      size={32} 
+      className={`pr-1 ${priority>2?"text-nav":"text-slate-400"}`}
+      />
+      <BellSimple 
+      size={32} 
+      className={`pr-1 ${priority>3?"text-nav":"text-slate-400"}`}
+      />
+      <BellSimple 
+      size={32} 
+      className={`pr-1 ${priority>4?"text-nav":"text-slate-400"}`}
+      />
+    </div>
+
+  )
+}
